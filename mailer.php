e810d65267fe1f9392fef65787405c728ed09063
amanohar17 <amanohar@wpi.edu>
2020-07-10 15:56:04 -0400

    added the .cpanel.yml file and mailer edits

--- mailer.php
+++ mailer.php
@@ -8,12 +8,12 @@
 
     // Check the data.
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: http://www.webdesigncourse.co/omnifood/index.php?success=-1#form");
+        header("Location: http://www.ajaymanohar.com/index.php?success=-1#form");
         exit;
     }
 
     // Set the recipient email address. Update this to YOUR desired email address.
-    $recipient = "<YOUR EMAIL HERE>";
+    $recipient = "<amanohar@wpi.edu>";
 
     // Set the email subject.
     $subject = "New contact from $name";
@@ -30,6 +30,6 @@
     mail($recipient, $subject, $email_content, $email_headers);
     
     // Redirect to the index.html page with success code
-    header("Location: http://www.webdesigncourse.co/omnifood/index.php?success=1#form");
+    header("Location: http://www.ajaymanohar.com/index.php?success=1#form");
 
 ?>
